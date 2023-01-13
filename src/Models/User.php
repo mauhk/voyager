f444c235dfe8a1a29a2e32a9b0c79d5e2d2207f6
mkretzmann <mauriciohk@gmail.com>
2023-01-13 13:38:29 -0300

    revert uuid

--- src/Models/User.php
+++ src/Models/User.php
@@ -53,36 +53,4 @@ class User extends Authenticatable implements UserContract
         return UserFactory::new();
     }
 
-    /**
-     * Boot function from Laravel.
-     */
-    protected static function boot()
-    {
-        parent::boot();
-        static::creating(function ($model) {
-            if (empty($model->{$model->getKeyName()})) {
-                $model->{$model->getKeyName()} = Uuid::uuid4()->toString();
-            }
-        });
-    }
-
-    /**
-     * Get the value indicating whether the IDs are incrementing.
-     *
-     * @return bool
-     */
-    public function getIncrementing()
-    {
-        return false;
-    }
-
-    /**
-     * Get the auto-incrementing key type.
-     *
-     * @return string
-     */
-    public function getKeyType()
-    {
-        return 'string';
-    }
 }
